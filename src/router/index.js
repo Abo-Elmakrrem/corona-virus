eb0f3fb7a1d52b23249a3f2064604c059fe01a34
Abo-Elmakrrem <ahmed_elshenawy_2@hotmail.com>
2021-11-28 21:54:16 +0200

    init app

--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,6 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Test from '../components/Test.vue'
-import About from '../components/About-Ammna.vue'
-import Symptoms from '../components/Symptoms-Mkhamer.vue'
-import Prevention from '../components/Prevention-Yousef.vue'
-import Treatment from '../components/Treatment-Barghot.vue'
-import FandQ from '../components/FandQ-Samka.vue'
-import News from '../components/News-Salama.vue'
-
 Vue.use(VueRouter)
 const routes = [
   {
@@ -20,36 +13,6 @@ const routes = [
     path: '/Test',
     name: 'Test',
     component: Test
-  },
-  {
-    path: '/About',
-    name: 'About',
-    component: About
-  },
-  {
-    path: '/Symptoms',
-    name: 'Symptoms',
-    component: Symptoms
-  },
-  {
-    path: '/Prevention',
-    name: 'Prevention',
-    component: Prevention
-  },
-  {
-    path: '/Treatment',
-    name: 'Treatment',
-    component: Treatment
-  },
-  {
-    path: '/FandQ',
-    name: 'FandQ',
-    component: FandQ
-  },
-  {
-    path: '/News',
-    name: 'News',
-    component: News
   }
 ]
 
