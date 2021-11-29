3fd233cecc5c56ce6a4e558d99993ea1ec3b7f51
Abo-Elmakrrem <ahmed_elshenawy_2@hotmail.com>
2021-11-29 17:03:14 +0200

    adding the css file

--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 // Import Bootstrap an BootstrapVue CSS files (order is important)
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import './assets/main.css'
 
 // Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue)
